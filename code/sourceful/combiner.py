3e784e27292c1351ad3a1c2d9bfae78e764d5741
Amitrajit Sarkar <aaiijmrtt@gmail.com>
2017-01-30 00:10:59 +0530

    better concatenation

--- code/sourceful/combiner.py
+++ code/sourceful/combiner.py
@@ -7,9 +7,9 @@ def create(encoder1, encoder2, config, scope = 'combiner'):
 	with tf.name_scope(scope):
 		for i in xrange(dim_d):
 			with tf.name_scope('layer_%i' %i):
-				model['cW_%i' %i] = tf.Variable(tf.random_uniform([2 * dim_i, dim_i], - np.sqrt(6. / dim_i), np.sqrt(6. / dim_i)), name = 'cW_%i' %i) if i == 0 else tf.Variable(tf.random_uniform([dim_i, dim_i], - np.sqrt(6. / dim_i), np.sqrt(6. / dim_i)), collections = [tf.GraphKeys.VARIABLES, tf.GraphKeys.REGULARIZATION_LOSSES], name = 'cW_%i' %i)
+				model['cW_%i' %i] = tf.Variable(tf.random_uniform([4 * dim_i, dim_i], - np.sqrt(6. / dim_i), np.sqrt(6. / dim_i)), name = 'cW_%i' %i) if i == 0 else tf.Variable(tf.random_uniform([dim_i, dim_i], - np.sqrt(6. / dim_i), np.sqrt(6. / dim_i)), collections = [tf.GraphKeys.VARIABLES, tf.GraphKeys.REGULARIZATION_LOSSES], name = 'cW_%i' %i)
 				model['cB_%i' %i] = tf.Variable(tf.random_uniform([1, dim_i], - np.sqrt(6. / dim_i), np.sqrt(6. / dim_i)), name = 'cB_%i' %i)
-				model['cx_%i' %i] = tf.concat(1, [encoder1['eh_%i' %(dim_t - 1)], encoder2['eh_%i' %(dim_t - 1)]], name = 'cx_%i' %i) if i == 0 else model['cy_%i' %(i - 1)]
+				model['cx_%i' %i] = tf.concat(1, [encoder1['eh_%i' %(dim_t - 1)], encoder2['eh_%i' %(dim_t - 1)], tf.sub(encoder1['eh_%i' %(dim_t - 1)], encoder2['eh_%i' %(dim_t - 1)]), tf.mul(encoder1['eh_%i' %(dim_t - 1)], encoder2['eh_%i' %(dim_t - 1)])], name = 'cx_%i' %i) if i == 0 else model['cy_%i' %(i - 1)]
 				model['cy_%i' %i] = tf.nn.sigmoid(tf.add(tf.matmul(model['cx_%i' %i], model['cW_%i' %i]), model['cB_%i' %i]), name = 'cy_%i' %i)
 
 	return model
--- code/sourceful/embedder.py
+++ code/sourceful/embedder.py
@@ -5,7 +5,7 @@ def create(config, scope = 'embedder', embedding = None):
 	model = dict()
 
 	with tf.name_scope(scope):
-		model['We'] = tf.Variable(tf.random_uniform([dim_v, dim_i], collections = [tf.GraphKeys.VARIABLES, tf.GraphKeys.REGULARIZATION_LOSSES], - np.sqrt(6. / dim_i), np.sqrt(6. / dim_i)), name = 'We') if embedding is None else tf.Variable(embedding, collections = [tf.GraphKeys.VARIABLES, tf.GraphKeys.REGULARIZATION_LOSSES], name = 'We')
+		model['We'] = tf.Variable(tf.random_uniform([dim_v, dim_i], - np.sqrt(6. / dim_i), np.sqrt(6. / dim_i)), collections = [tf.GraphKeys.VARIABLES, tf.GraphKeys.REGULARIZATION_LOSSES], name = 'We') if embedding is None else tf.Variable(embedding, collections = [tf.GraphKeys.VARIABLES, tf.GraphKeys.REGULARIZATION_LOSSES], name = 'We')
 		model['Be'] = tf.Variable(tf.random_uniform([1, dim_i], - np.sqrt(6. / dim_i), np.sqrt(6. / dim_i)), name = 'Be')
 
 	return model

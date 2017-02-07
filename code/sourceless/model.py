772ce71293322e777d55ad097d3eca9c38065960
Amitrajit Sarkar <aaiijmrtt@gmail.com>
2017-02-08 01:47:16 +0530

    pretraining: sourceless

--- code/sourceless/model.py
+++ code/sourceless/model.py
@@ -1,4 +1,4 @@
-import sys, configparser, datetime
+import sys, configparser, datetime, signal
 import encoder, bicoder, decoder, atcoder, alcoder
 import tensorflow as tf, numpy as np
 
@@ -12,7 +12,7 @@ def postpad(unpadded, pad, size):
 		return unpadded
 	return unpadded + [pad] * (size - len(unpadded))
 
-def feed(model, config, filename):
+def feed(model, pretrainer, config, filename):
 	batch, length, align = config.getint('global', 'batchsize'), config.getint('global', 'timesize'), config.getboolean('global', 'align')
 	inilist, prelist, postlist, alignlist = list(), list(), list(), list()
 	for line in open(filename):
@@ -30,27 +30,44 @@ def feed(model, config, filename):
 					for idx in indexalign:
 						calign[-1][ii][idx + length - lengths[ii]] = 1. / len(indexalign)
 			alignlist.append(calign)
-		prelist.append(prepad([int(pre) for pre in preedit.split()], 0, length))
-		postlist.append(postpad([int(post) for post in postedit.split()], 0, length))
+		prelist.append(prepad([int(pre) for pre in preedit.split()] + [0], 0, length))
+		postlist.append(postpad([int(post) for post in postedit.split()] + [0], 0, length))
 		if len(prelist) == batch:
 			feeddict = dict()
 			feeddict.update({model['exi_%i' %i]: [prelist[ii][i] for ii in xrange(batch)] for i in xrange(length)})
+			feeddict.update({model['eyi_%i' %i]: [prelist[ii][i + 1] for ii in xrange(batch)] for i in xrange(length - 1)})
 			feeddict.update({model['dyi_%i' %i]: [postlist[ii][i] for ii in xrange(batch)] for i in xrange(length)})
+			feeddict.update({pretrainer['exi_%i' %i]: [prelist[ii][i] for ii in xrange(batch)] for i in xrange(length)})
+			feeddict.update({pretrainer['eyi_%i' %i]: [prelist[ii][i + 1] for ii in xrange(batch)] for i in xrange(length - 1)})
 			if align:
 				feeddict.update({model['dai_%i' %i]: [[alignlist[ii][i][1][iii] for ii in xrange(batch)] for iii in xrange(length)] for i in xrange(length)})
 			yield feeddict
 			inilist, prelist, postlist, alignlist = list(), list(), list(), list()
 
-def run(model, config, session, summary, filename, train):
-	iters, freq, time, saves, align, total, totaldmss, totaldmsa = config.getint('global', 'iterations') if train else 1, config.getint('global', 'frequency'), config.getint('global', 'timesize'), config.get('global', 'output'), config.getboolean('global', 'align'), 0., 0., 0.
-	if config.get('global', 'lfunc') == 'mse': ds, td, sds, gsd = model['dmses'], model['tdmses'], model['sdmses'], model['gsd']
-	if config.get('global', 'lfunc') == 'nll': ds, td, sds, gsd = model['dnlls'], model['tdnlls'], model['sdnlls'], model['gsd']
+def run(model, pretrainer, config, session, summary, filename, train):
+	iters, freq, time, saves, align, total, totalemss, totaldmss, totaldmsa = config.getint('global', 'iterations') if train else 1, config.getint('global', 'frequency'), config.getint('global', 'timesize'), config.get('global', 'output'), config.getboolean('global', 'align'), 0., 0., 0., 0.
+	if config.get('global', 'lfunc') == 'mse': es, te, ses, gse, ds, td, sds, gsd, ps, tp, sps, gsp = model['emses'], model['temses'], model['semses'], model['gse'], model['dmses'], model['tdmses'], model['sdmses'], model['gsd'], pretrainer['emses'], pretrainer['semses'], pretrainer['edmses'], pretrainer['gse']
+	if config.get('global', 'lfunc') == 'nll': es, te, ses, gse, ds, td, sds, gsd, ps, tp, sps, gsp  = model['enlls'], model['tenlls'], model['senlls'], model['gse'], model['dnlls'], model['tdnlls'], model['sdnlls'], model['gsd'], pretrainer['enlls'], pretrainer['senlls'], pretrainer['ednlls'], pretrainer['gse']
 	if config.get('global', 'lfunc') == 'mse' and align: da, sda = model['dmsea'], model['sdmsea']
 	if config.get('global', 'lfunc') == 'nll' and align: da, sda = model['dmsea'], model['sdmsea']
 
 	for i in xrange(iters):
-		if train:
-			for ii, feeddict in enumerate(feed(model, config, filename)):
+		for ii, feeddict in enumerate(feed(model, pretrainer, config, filename)):
+			if train == 'pretrain2':
+				valemss, t = session.run([es, te], feed_dict = feeddict)
+				totalemss += valemss
+				if (ii + 1) % freq == 0:
+					summs = session.run(ses, feed_dict = feeddict)
+					summary.add_summary(summs, gse.eval())
+					print datetime.datetime.now(), 'iteration', i, 'batch', ii, 'loss:', 'seq', totalemss
+			if train == 'pretrain3':
+				valemss, t = session.run([ps, tp], feed_dict = feeddict)
+				totalemss += valemss
+				if (ii + 1) % freq == 0:
+					summs = session.run(sps, feed_dict = feeddict)
+					summary.add_summary(summs, gsp.eval())
+					print datetime.datetime.now(), 'iteration', i, 'batch', ii, 'loss:', 'seq', totalemss
+			elif train == 'train':
 				if align:
 					valdmss, valdmsa, t = session.run([ds, da, td], feed_dict = feeddict)
 					totaldmss += valdmss
@@ -67,8 +84,7 @@ def run(model, config, session, summary, filename, train):
 						summ = session.run(sds, feed_dict = feeddict)
 						summary.add_summary(summ, gsd.eval())
 						print datetime.datetime.now(), 'iteration', i, 'batch', ii, 'loss', total
-		else:
-			for ii, feeddict in enumerate(feed(model, config, filename)):
+			elif train == 'dev' or train == 'test':
 				val = session.run([model['dp_%i' %iii] for iii in xrange(time)], feed_dict = feeddict)
 				exps, vals, outs = [feeddict[model['dyi_%i' %iii]] for iii in xrange(time)], [x[0] for x in val], [x[1] for x in val]
 				np.savez(open('%s/%i' %(saves, ii), 'w'), values = np.transpose(np.array(vals), [1, 0, 2]), outputs = np.transpose(np.array(outs), [1, 0, 2]))
@@ -77,12 +93,20 @@ def run(model, config, session, summary, filename, train):
 						if exp == 0: continue
 						if exp in out: total += 1
 
-	if train and align: return totaldmss, totaldmsa
+	if train == 'pretrain': return totalemss
+	elif train == 'train' and align: return totaldmss, totaldmsa
 	else: return total
+
+def handler(signum, stack):
+	print datetime.datetime.now(), 'terminating execution'
+	print datetime.datetime.now(), 'saving model'
+	tf.train.Saver().save(sess, config.get('global', 'save'))
+	sys.exit()
 
 if __name__ == '__main__':
 	config = configparser.ConfigParser(interpolation = configparser.ExtendedInterpolation())
 	config.read(sys.argv[1])
+	signal.signal(signal.SIGINT, handler)
 
 	print datetime.datetime.now(), 'creating model'
 	embedding, model, mode = np.loadtxt('%s/model' %config.get('global', 'data')).astype(np.float32), dict(), [int(sys.argv[2]) % 2, (int(sys.argv[2]) / 2) % 3]
@@ -91,17 +115,18 @@ if __name__ == '__main__':
 	if mode[1] == 0: model = decoder.create(model, config['decoder'])
 	if mode[1] == 1: model = atcoder.create(model, config['atcoder'])
 	if mode[1] == 2: model = alcoder.create(model, config['alcoder'])
+	pretrainer = encoder.create(dict(), config['encoder'], embedder)
 
 	with tf.Session() as sess:
-		sess.run(tf.initialize_all_variables())
-#		tf.train.Saver().restore(sess, config.get('global', 'load'))
-		summary = tf.train.SummaryWriter(config.get('global', 'logs'), sess.graph)
-
-		print datetime.datetime.now(), 'training model'
-		trainingloss = run(model, config, sess, summary, '%s/train' %config.get('global', 'data'), True)
-		print datetime.datetime.now(), 'training loss', trainingloss
+		if sys.argv[3] == 'init':
+			sess.run(tf.initialize_all_variables())
+		else:
+			tf.train.Saver().restore(sess, config.get('global', 'load'))
+			if sys.argv[3] == 'retrain': sys.argv[3] = 'train'
+			elif sys.argv[3] == 'train': sess.run([decoder_['dW%s_%i' %(descriptor, depth)].assign(_decoder['eW%s_%i' %(descriptor, depth)]) for depth in xrange(config.getint('global', 'depth')) for descriptor in ['i', 'f', 'o', 'c', 'z']])
+			summary = tf.train.SummaryWriter(config.get('global', 'logs'), sess.graph)
+			print datetime.datetime.now(), 'running model'
+			returnvalue = run(model, pretrainer, config, sess, summary, '%s/%s' %(config.get('global', 'data'), sys.argv[3]), sys.argv[3])
+			print datetime.datetime.now(), 'returned value', returnvalue
 		print datetime.datetime.now(), 'saving model'
 		tf.train.Saver().save(sess, config.get('global', 'save'))
-		print datetime.datetime.now(), 'testing model'
-		testingaccuracy = run(model, config, sess, summary, '%s/dev' %config.get('global', 'data'), False)
-		print datetime.datetime.now(), 'testing accuracy', testingaccuracy
